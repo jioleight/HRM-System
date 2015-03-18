fa922dbbdf783f3ba56e01650d65e7935665f0df
Oji Leightton Vynn-Z Bucad <jiofreed01@gmail.com>
2015-03-19 04:00:31 +0800

    Working GUI for login
    used graphics input

--- MAIN.C
+++ MAIN.C
@@ -3,33 +3,158 @@
  *	Programmer: Oji Leightton Vynn-Z Bucad
  *	Date Created: 03/18/2015
  */
-
-
-
+ 
 #include<graphics.h>
 #include<conio.h>
+#include<dos.h>
+#include<stdlib.h>
+#define MAX_IN_LEN 9
+#define MAX_IN_LEN_P 16
 
-GUI()
+LOGUI()
 {
-	int gd = DETECT,gm,left=100,top=100,right=200,bottom=200,x= 300,y=150,radius=50;
- 
+	char user[MAX_IN_LEN], pass[MAX_IN_LEN_P];
+	int gd = DETECT, gm, c, left = 200, top = 150, right = 450, bottom = 250, in_s = 0, in_e = 0;
 	initgraph(&gd, &gm, "s:\\turboc\\BGI");
-	setcolor(BLUE);
-	rectangle(left, top, right, bottom);
-	circle(x, y, radius);
-	bar(left + 300, top, right + 300, bottom);
-	line(left - 10, top + 150, left + 410, top + 150);
-	ellipse(x, y + 200, 0, 360, 100, 50);
-	outtextxy(left + 100, top + 325, "My First C Graphics Program");
 	
+	cleardevice();
+	
+	setbkcolor(BLUE);
+	/*Main pane*/
+	setfillstyle(SOLID_FILL, LIGHTGREEN);
+	bar(140, 90, 510, 300);
+	/*Title Bar*/
+	setfillstyle(SOLID_FILL, LIGHTGRAY);
+	bar(140, 90, 510, 115);
+	/*Log-in pane*/
+	setfillstyle(SOLID_FILL, LIGHTBLUE);
+	bar(left, top, right, bottom);
+	/*Log-in Field*/
+	setfillstyle(SOLID_FILL, WHITE);
+	bar(250, 175, 400, 190);
+	/*Password Field*/
+	setfillstyle(SOLID_FILL, WHITE);
+	bar(250, 225, 400, 240);
+	
+	setcolor(WHITE);
+	settextstyle(GOTHIC_FONT, HORIZ_DIR, 3);
+	rectangle(140, 90, 510, 300);
+	
+	setcolor(DARKGRAY);
+	outtextxy(150, 100, "WELCOME TO HUMAN RESOURCES MANAGEMENT SYSTEM");
+	
+	setcolor(WHITE);
+	outtextxy(210, 160, "LOG-IN AS: ");
+	outtextxy(210, 210, "PASSWORD: ");
+	
+	/*Cleaner*/
+	for(c = 0; c < MAX_IN_LEN && MAX_IN_LEN_P; c++)
+	{
+		user[c] = NULL;
+		pass[c] = NULL;
+	}
+	
+	/*LOGIN FIELD*/
+	do
+	{
+		setcolor(RED);
+		outtextxy(255, 180, user);
+		c = getche();
+		switch (c)
+		{
+			case 8: /* backspace */
+				if (in_s)
+				{
+					in_s--;
+					user[in_s] = 0;
+					/*Log-in Field*/
+					setfillstyle(SOLID_FILL, WHITE);
+					bar(250, 175, 400, 190);
+				}
+				break;
+			case 13: /* return */
+				in_e = 1;
+				break;
+			case 27: /* Escape = Abort */
+				user[0] = 0;
+				in_e = 2;
+				break;
+			default:
+				if (in_s < MAX_IN_LEN - 1 && c >= ' ' && c <= '~')
+				{
+				user[in_s] = c;
+				in_s++;
+				user[in_s] = 0;
+				}
+		}
+	}while(!in_e);
+	
+	/*Aborting Half-way*/
+	if(in_e == 2)return 0;
+	
+	/*Set to NULL to avoid errors*/
+	in_e = NULL;
+	in_s = NULL;
+	c = NULL;
+	
+	/*PASS FIELD*/
+	do
+	{
+		setcolor(RED);
+		outtextxy(255, 230, pass);
+		c = getche();
+		switch (c)
+		{
+			case 8: /* backspace */
+				if (in_s)
+				{
+					in_s--;
+					pass[in_s] = 0;
+					/*Password Field*/
+					setfillstyle(SOLID_FILL, WHITE);
+					bar(250, 225, 400, 240);
+				}
+				break;
+			case 13: /* return */
+				in_e = 1;
+				break;
+			case 27: /* Escape = Abort */
+				pass[0] = 0;
+				in_e = 2;
+				break;
+			default:
+				if (in_s < MAX_IN_LEN_P - 1 && c >= ' ' && c <= '~')
+				{
+				pass[in_s] = c;
+				in_s++;
+				pass[in_s] = 0;
+				}
+		}
+	}while(!in_e);
+	
+	LOGIN(user, pass);
+}
+MENUGUI()
+{
+	
+}
+
+LOGIN(char user[MAX_IN_LEN], int pass[MAX_IN_LEN_P])
+{
+	puts(user);
+	puts(pass);
 }
 
 main()
 {
-   GUI();
- 
-   getch();
-   closegraph();
-   return 0;
+	char user[8], pass[16];
+	int count, input;
+	clrscr();
+	
+	LOGUI();
+
+	getch();
+	closegraph();
+	return 0;
 }
 
