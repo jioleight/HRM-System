d3753d45102371906ee539a8d4b1a8944347576e
Oji Leightton Vynn-Z Bucad <jiofreed01@gmail.com>
2015-03-23 20:51:00 +0800

    Implemented pointers
    pointers for user for a more secure transition from function to function
    without losing the value in the variable

--- MAIN.C
+++ MAIN.C
@@ -23,7 +23,7 @@ main()
 LOGUI()
 {
 	char user[MAX_IN_LEN], pass[MAX_IN_LEN_P], hid[MAX_IN_LEN_P];
-	int gd = DETECT, gm, c, in_s = 0, in_e = 0;;
+	int gd = DETECT, gm, c, in_s = 0, in_e = 0;
 	initgraph(&gd, &gm, "s:\\turboc\\BGI");
 	
 	cleardevice();
@@ -171,13 +171,15 @@ LOGUI()
 	}
 	LOGIN(user, pass);
 }
-MENUGUI(char user[MAX_IN_LEN], char pass[MAX_IN_LEN_P])
+MENUGUI(char *user[MAX_IN_LEN])
 {
-	int i = 0, y1 = 0, y2 = 0, usr = NULL, c;
-	cleardevice();
-	
+	int i = 0, y1 = 0, y2 = 0, usr = NULL, c, s = 1;
+	char k = ' ';
+	FILE *f,*t; 
+	INFO *new=NULL,*start=NULL,*tmp, data; 
+	INFO **pnew=&new,**pstart=&start;
 	usr = strcmp(user, "admin");
-	
+	cleardevice();
 	/*Main pane*/
 	setfillstyle(SOLID_FILL, LIGHTGREEN);
 	bar(50, 50, 600, 450);
@@ -209,6 +211,7 @@ MENUGUI(char user[MAX_IN_LEN], char pass[MAX_IN_LEN_P])
 	{
 		setcolor(WHITE);
 		settextstyle(DEFAULT_FONT, HORIZ_DIR, 1);
+		outtextxy(33, 2, user);
 		if(usr == 0)
 		{
 			outtextxy(90, 130, "SELECTION");
@@ -223,6 +226,8 @@ MENUGUI(char user[MAX_IN_LEN], char pass[MAX_IN_LEN_P])
 			if(c == 80 && usr == 0 && i != 7)
 			{	
 				/*Selector Cursor*/
+				setcolor(RED);
+				rectangle(85, 145+y1, 205, 160+y2);
 				setcolor(LIGHTBLUE);
 				rectangle(85, 145+y1-20, 205, 160+y2-20);
 				setcolor(RED);
@@ -238,11 +243,18 @@ MENUGUI(char user[MAX_IN_LEN], char pass[MAX_IN_LEN_P])
 				y1 = y1 - 20;
 				y2 = y2 - 20;
 				/*Selector Cursor*/
+				setcolor(RED);
+				rectangle(85, 145+y1, 205, 160+y2);
 				setcolor(LIGHTBLUE);
 				rectangle(85, 145+y1+20, 205, 160+y2+20);
 				setcolor(RED);
 				rectangle(85, 145+y1, 205, 160+y2);
 				i--;
+			}
+			if(c == 27)
+			{
+				i = 8;
+				break;
 			}
 		}
 		else if(usr != 0)
@@ -276,12 +288,48 @@ MENUGUI(char user[MAX_IN_LEN], char pass[MAX_IN_LEN_P])
 				rectangle(85, 145+y1, 205, 160+y2);
 				i--;
 			}
+			if(c == 27)
+			{
+				i = 8;
+				break;
+			}
 		}
 		c = getch();
 	}while(c != 13);
 	
 	while(i != 8)
 	{
+		/************************************IMPORT********************************/
+		f=fopen("INFO.DAT","r");
+		while(s!=3)
+		{		
+			new=(INFO *)malloc(sizeof(INFO));
+			if((fscanf(f,"%[^\n]\ns \n",(*pnew)->user_name))!=EOF)
+			{	
+				fscanf(f,"%[^\n]\ns \n",(*pnew)->first_name);
+				fscanf(f,"%[^\n]\ns \n",(*pnew)->last_name);
+				fscanf(f,"%[^\n]\ns \n",(*pnew)->gender);
+				fscanf(f,"%[^\n]\ns \n",(*pnew)->tel_no);
+				fscanf(f,"%[^\n]\ns \n",(*pnew)->email);
+				fscanf(f,"%[^\n]\ns \n\n",(*pnew)->address);
+				if(s==1) 
+				{
+					create(&pnew,&pstart); 
+					s=0;
+				}
+				else 
+					insert(&pnew,start);
+			}
+			else
+			{
+				free(new); 
+				s=3;
+			}
+		}	
+		fclose(f);
+		
+		tmp = start;
+		/*******************************************************************************/
 		switch(i)
 		{
 			case 1:
@@ -296,8 +344,10 @@ MENUGUI(char user[MAX_IN_LEN], char pass[MAX_IN_LEN_P])
 				break;
 			case 2:
 				if(usr == 0)
-				{
-					
+				{	
+					i = 8;
+					add(&new,&start, &user);
+					break;
 				}
 				else
 				{
@@ -324,9 +374,26 @@ MENUGUI(char user[MAX_IN_LEN], char pass[MAX_IN_LEN_P])
 			case 7:
 				outtextxy(10,10, "[x] EXIT");
 				return 0;
+				break;
 			default:
 				break;
 		}
+		t=fopen("tmp.dat","a");
+		tmp=start;
+		while(tmp!=NULL)
+		{
+			fprintf(t,"%s\n",tmp->user_name);
+			fprintf(f,"%s\n",tmp->first_name);
+			fprintf(f,"%s\n",tmp->last_name);
+			fprintf(f,"%s\n",tmp->gender);
+			fprintf(f,"%s\n",tmp->tel_no);
+			fprintf(f,"%s\n",tmp->email);
+			fprintf(f,"%s\n\n",tmp->address);
+			tmp=tmp->n;
+		}	
+		fclose(t); 
+		remove("INFO.DAT"); 
+		rename("tmp.dat","INFO.DAT");
 	}
 }
 
@@ -352,7 +419,7 @@ void LOGIN(char user[MAX_IN_LEN], char pass[MAX_IN_LEN_P])
 		if(strcmp(user, tmp_user) == 0 && strcmp(pass, tmp_pass) == 0)
 		{
 			fclose(fp);
-			MENUGUI(tmp_user, tmp_pass);
+			MENUGUI(&tmp_user);
 			break;
 		}
 		/*Remember to put space after the last acc to trap*/
@@ -368,5 +435,4 @@ void LOGIN(char user[MAX_IN_LEN], char pass[MAX_IN_LEN_P])
 	}
 	fclose(fp);
 }
-
 
